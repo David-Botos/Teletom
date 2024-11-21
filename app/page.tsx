cb834bf98dd636e31903256005773745a0f8b3c4
David Botos <dbotos@umich.edu>
2024-11-21 15:55:54 -0800

    available height hook and height fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+'use client'
+import { useRef } from "react";
 import WebRTCClient from "./_components/CallInterface/webRTCClient";
 import { CalendarComponent } from "./_components/dndCalendar";
+import { useAvailableHeight } from "./_hooks/useAvailableHeight";
 
 export default function Home() {
+  const navRef = useRef<HTMLDivElement>(null);
+  const availableHeight = useAvailableHeight(navRef);
+
   return (
     <div className="flex flex-col h-screen">
-      {/* <div className="flex justify-between bg-slate-50 py-4 px-6">
+      <div className="flex justify-between bg-slate-50 py-4 px-6" ref={navRef}>
         <div>
           <p>TAPN Demo</p>
         </div>
-        <div>
-          <p className="text-blue-600 font-bold">Start Call</p>
-        </div>
-      </div> */}
+        <div>{/* <p className="text-blue-600 font-bold">Start Call</p> */}</div>
+      </div>
       {/* calendar / call interface */}
-      <div className="flex h-full gap-5 m-10">
+      <div className="flex gap-5 p-10" style={{ height: availableHeight }}>
         <div className="h-full w-3/5 flex items-center">
           <div className="flex overflow-hidden w-full h-full">
             <CalendarComponent />
